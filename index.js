018166472cb1e21e498ff47c464d0166dab76baf
Gilles Doge <gilles@antistatique.net>
2017-11-12 22:43:35 +0100

    Fetch data from peoplefone

--- index.js
+++ index.js
@@ -1,59 +1,101 @@
 require('dotenv').load();
 
-var express = require('express');
-var request = require("request-with-cookies");
+const express = require('express');
+const request = require('request-with-cookies');
+const cheerio = require('cheerio')
 
-var app = express();
+const app = express();
 
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 // app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 })); 
 
-var port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
+
+function fetchPhoneNumbers(callback) {
+  const client = request.createClient({ followRedirect: false, followAllRedirects: false });
+  const loginUrl = 'https://me.peoplefone.ch/en/home/index/';
+  const userUrl = 'https://me.peoplefone.ch/en/pbxusers/users/';
+
+  const formData = {
+    //'_token': token,
+    'logincode': '',
+    'loginuser': process.env.PEOPLEFONE_USER,
+    'loginpass': process.env.PEOPLEFONE_PASSWORD
+  };
+
+  // login
+  client.post({ url: loginUrl, form: formData }, function(err, response, body) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    // extract phone list
+    client(userUrl, function (err, response, body) {
+      const $ = cheerio.load(body);
+      const phones = [];
+      $('div#content tr').each(function(i, el) {
+        const number = $(el).find('td:nth-child(2)').text();
+        const name = $(el).find('td:nth-child(3)').text();
+        const online = $(el).find('td:nth-child(4)').attr('class') === 'color_green' ? true : false;
+
+        phones.push({
+          'name': name,
+          'shortPhone': number,
+          'online': online
+        });
+      });
+
+      callback(null, phones);
+    });
+  });
+}
 
 app.get('/', function (req, res) {
-  res.send('hi.');
+  res.send('hi!');
 });
 
 app.post('/slack/telephones', function (req, res) {
-
-  console.log(req.body);
 
   if (req.body.token !== process.env.SLACK_TOKEN) {
     return res.status(401).send('Unauthorized Slack account');
   }
 
-  let phones = [
-    {
-      'name': 'Gilles',
-      'shortPhone': 22,
-      'online': true,
+  const client = request.createClient();
+
+  // Fetch the csrf token
+  fetchPhoneNumbers(function (err, phones) {
+
+    if (err) {
+      res.status(500).send('Error while fetching phone data: ' + err);
+      return;
     }
-  ];
 
+    // Format response
+    const fields = phones.map(function(phone) {
+      const onlineEmoji = phone.online ? ":white_check_mark:" : ':x:';
+      return {
+        'title': phone.name,
+        'value': `${phone.shortPhone} ${onlineEmoji}`,
+        'short': true,
+      }
+    });
 
-  const fields = phones.map(function(phone) {
-    const onlineEmoji = phone.online ? ":white_check_mark:" : ':x:';
-    return {
-      'title': phone.name,
-      'value': `${phone.shortPhone} ${onlineEmoji}`,
-      'short': true,
-    }
-  });
-
-  const attachments = {
-    "attachments": [
+    const attachments = {
+      "attachments": [
         {
-            "text": "Liste des numéros de téléphone:",
-            "fields": fields,
-            "color": "#F35A00"
+          "text": "Liste des numéros de téléphone:",
+          "fields": fields,
+          "color": "good"
         }
-    ]
-}
+      ]
+    };
 
-  res.status(200).set('Content-Type', 'application/json').send(JSON.stringify(attachments));
+    res.status(200).set('Content-Type', 'application/json').send(JSON.stringify(attachments));
+  });
 });
 
 app.get('/health_check', function (req, res) {
